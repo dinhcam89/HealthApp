d37b1f41804402ed03602cd61d003758d469d1ee
dcamm79 <dinhcam1512@gmail.com>
2023-11-14 17:42:56 +0700

    done - sign up, sign in with firebase

---
+++ app/src/main/java/com/example/healthapp/SignUpActivity.java
@@ -0,0 +1,97 @@
+package com.example.healthapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextPwd;
+    ImageButton btnSignUp;
+    FirebaseAuth mailAuth;
+    TextView textViewSignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        editTextEmail = findViewById(R.id.editText_Email);
+        editTextPwd = findViewById(R.id.editText_Password);
+        btnSignUp = findViewById(R.id.btn_SignUp);
+        mailAuth = FirebaseAuth.getInstance();
+        textViewSignIn = findViewById(R.id.textView_SignIn);
+        textViewSignIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String email, pwd;
+                email = editTextEmail.getText().toString();
+                pwd = editTextPwd.getText().toString();
+                if(TextUtils.isEmpty((email)))
+                {
+
+                    return;
+                }
+                if(TextUtils.isEmpty(pwd))
+                {
+
+                    return;
+                }
+                mailAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    FirebaseUser user = mailAuth.getCurrentUser();
+                                    Toast.makeText(SignUpActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else
+                                {
+                                    Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+
+                        });
+
+            }
+        });
+
+    }
+}
