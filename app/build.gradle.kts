ecaa3709f62489a0b19c5677a487ff1e62eec617
quynga <21521169@gm.uit.edu.vn>
2023-12-23 11:34:44 +0700

    bottom navigation menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,14 @@ android {
             )
         }
     }
+
+/*    viewBinding {
+        enabled = true
+    }*/
+
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
