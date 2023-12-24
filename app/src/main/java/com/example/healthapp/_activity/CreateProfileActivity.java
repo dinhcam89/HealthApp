7e25ede3906e3f387be5913caf5ffd29758d9197
quynga <21521169@gm.uit.edu.vn>
2023-12-24 12:06:14 +0700

    create profile

---
+++ app/src/main/java/com/example/healthapp/_activity/CreateProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthapp._activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthapp.R;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
--- app/src/main/java/com/example/healthapp/_activity/MainMenuActivity.java
+++ app/src/main/java/com/example/healthapp/_activity/MainMenuActivity.java
@@ -2,6 +2,9 @@ package com.example.healthapp._activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,9 +13,13 @@ import android.widget.Button;
 
 import com.example.healthapp.R;
 import com.example.healthapp._activity.DoctorChoosingActivity;
+import com.example.healthapp.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     /*Button btnSignOut;
     FirebaseAuth emailAuth;
@@ -47,7 +54,53 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         setContentView(R.layout.activity_main_menu);
+
+       /* replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.booking:
+                    replaceFragment (new BookingFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new Profileragment());
+                    break;
+
+            }
+
+            return true;
+        });*/
+
+       /* private void replaceFragment(Fragment fragment){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            FragmentTransaction.replace(R.id.frame_layout, fragment);
+            FragmentTransaction.commit();
+
+        }*/
+        /*private void replaceFragment(Fragment fragment){
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+
+            fragmentTransaction.commit();
+
+        }*/
+
+
+
+        /*setContentView(R.layout.activity_main_menu);*/
 
         Bacsi = findViewById(R.id.bacsi);
         intent_bacsi = new Intent(this, DoctorChoosingActivity.class);
